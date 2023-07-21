941cb214880b756b4586a84447cbca1e5e463c5e
Shohei2000 <77315803+Shohei2000@users.noreply.github.com>
2023-07-21 10:01:58 +0900

    Revert "オートコンプリート全ページ対応"

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -17,19 +17,39 @@ class JobController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function showJobPosts(Request $request)
-    {
-        $companies = Company::query();
+    // public function showJobPosts()
+    // {
 
-        $keyword = $request->input('keyword');
-        // キーワードが指定されている場合は検索処理を実行
-        if (!empty($keyword)) {
-            $companies->where('company_name', 'LIKE', '%' . $keyword . '%')
-            ->orwhere('company_name_kana', 'LIKE', '%' . $keyword . '%');
-        }
+    //     $companies = Company::all();
+    //     $bookmarks = Bookmark::where('user_id', Auth::user()->id)->get();
 
-        $companies = $companies->get();
+    //     return view('jobs.job_posts', compact('companies', 'bookmarks'));
+    // }
 
-    }
+    // // 企業詳細画面の表示
+    // public function showJobPostDetail(Company $company){
+
+    //      // 企業に関連するjobsのデータを取得
+
+    //     $jobs = Job::where('jobs.company_id', $company->id)
+    //                 ->get();
+        
+    //     return view('jobs.job_detail', compact('company', 'jobs'));
+    // }
+
+    // 企業求人詳細画面の表示
+    public function showJobDetail(Company $company, Job $job){
+
+    //     企業に関連するjobsのデータを取得
+    //    $jobs = Job::where('jobs.company_id', $company->id)
+    //                ->get();
+
+        $bookmarks = Bookmark::where('user_id', Auth::user()->id)->get();
+        $active = "JobDetail";
+       
+       return view('jobs.job_detail', compact('company', 'job', 'bookmarks', 'active'));
+   }
+
+    
 
 }
