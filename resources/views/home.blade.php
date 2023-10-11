d0b7f88dd426fa9fc43ab6401d045ab3888218eb
shohei22 <toyosima11031103@hotmail.co.jp>
2023-10-11 09:51:01 +0900

    FontawesomeのCSS読み込み

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://kit.fontawesome.com/916743b2a8.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>@include('header')</header>
