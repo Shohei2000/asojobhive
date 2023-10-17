830751d8ea40922f5dffa9280acb2a80e12c6ba6
shohei22 <toyosima11031103@hotmail.co.jp>
2023-10-17 12:42:08 +0900

    カレンダーのCSS変更/土日の色を変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/calendar.css') }}" rel="stylesheet">
     <!-- Script -->
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
