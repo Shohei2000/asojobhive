d78c269ea132233bfd0ca9e40ab146116d7430f1
shohei22 <toyosima11031103@hotmail.co.jp>
2023-10-17 09:51:56 +0900

    CSS変更/学校求人情報URLの折返し

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ホーム</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
     <!-- ビューファイル内の<head>セクション内に以下のスクリプトを追加 -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Script -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <header>@include('header')</header>
@@ -31,7 +36,7 @@
             <div class="col-6 h-100">
                 <div class="row w-100 h-100 d-flex justify-content-end">
                     <div class="col-12 h-100 border rounded-1" style="width:90%;">
-                        
+                        @include('calendar')
                     </div>
                 </div>
             </div>
