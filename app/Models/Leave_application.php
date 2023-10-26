c5e3d85f1765714bfad7ea6edc7b6af8add8b389
Aso2001011 <83050449+Aso2001011@users.noreply.github.com>
2023-10-26 10:31:43 +0900

    モデル再生成

--- app/Models/Leave_application.php
+++ app/Models/Leave_application.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Leave_application extends Model
+class leave_application extends Model
 {
     use HasFactory;
-
-    protected $table = 'leave_application';
 }
