cbab03f04beb8ea17008db1e81e0c86c73323759
Shohei2000 <toyosima11031103@hotmail.co.jp>
2023-12-05 10:18:55 +0900

    CORS設定：Ver.7.0 以降は[ /config/cors.php ]にてCORS設定をする

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,31 @@
 <?php
-
 return [
+  // CORSヘッダーを出力するパスのパターン、任意でワイルドカード(*)が利用できる。
+  //全てのルートを対象にする場合: ['*']
+  //APIと特定の画像を対象にする例: ['api/*', 'resources/example.png']
+ 'paths' => ['*'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+  // マッチするHTTPメソッド。 `[*]` だと全てのリクエストにマッチする。
+  //GETとPOSTだけを許可する場合: ['GET', 'POST']
+ 'allowed_methods' => ['*'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+  // 許可するリクエストオリジンの設定
+  //`*`かオリジンに完全一致、またはワイルドカードが利用可。
+ 'allowed_origins' => ['*'],
 
-    'allowed_methods' => ['*'],
+  //正規表現によるオリジン指定。preg_matchの引数としてそのまま渡される。
+ 'allowed_origins_patterns' => [],
 
-    'allowed_origins' => ['*'],
+ // Access-Control-Allow-Headers response header　レスポンスヘッダーの指定
+ 'allowed_headers' => ['*'],
 
-    'allowed_origins_patterns' => [],
+ //Access-Control-Expose-Headers レスポンスヘッダーの指定
+ 'exposed_headers' => false,
 
-    'allowed_headers' => ['*'],
+ //Access-Control-Max-Age レスポンスヘッダーの指定
+ 'max_age' => false,
 
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
-
+ // Access-Control-Allow-Credentialsヘッダーを設定する。
+ //falsy値を指定すると出力せず、truthyな値を渡せばtrueが出力される
+ 'supports_credentials' => false,
 ];
